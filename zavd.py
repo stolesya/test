936cc9f96b48b98230899ae4b0036b30164c67df
Olesya <olesya.stychuk@gmail.com>
2019-11-17 19:45:41 +0200

    meni vydae tut pomylku

---
+++ zavd.py
@@ -0,0 +1,17 @@
+a=float(input('введіть дійсне число: '))
+b=float(input('введіть дійсне число: '))
+c=float(input('введіть дійсне число: '))
+if a>b and a>c
+    print('найбільше число а')
+elif b>a and b>c:
+    print('найбільше число b')
+elif c>a and c>d:
+    print('найбільше число с')
+elif a==b and a>c:
+    print('a і b - рівні та більші c')
+elif b==c and b>a:
+    print('c і b - рівні та більші a')
+elif c==a and c>b: 
+    print('c і a - рівні та більші b')
+else:
+    print('усі числа рівні')
