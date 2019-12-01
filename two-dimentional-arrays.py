17f710c3616ff0bf90cf0e13b2c2d25f8c33ac53
Olesya <olesya.stychuk@gmail.com>
2019-12-01 12:35:23 +0200

    tda2

--- two-dimentional-arrays.py
+++ two-dimentional-arrays.py
@@ -2,12 +2,22 @@
 #[[0 for j in range(m)] for i in range(n)]
 import sys
 import random
-col=5
-row=3
+col=3
+row=5
 arr=[[random.randint(0, 9) for j in range(col)] for i in range(row)]
 print(arr)
 
+sum=0
+mul=1
+count0=0
 for i in range(row):
     for j in range(col):
-        sys.stdout.write(str(arr[i][j]))
+        el=arr[i][j]
+        sys.stdout.write(str(el))
+        sum+=el
+        if el!=0: 
+            mul*=el
+        else:
+            count0+=1
     print()
+print(sum, mul, count0)
